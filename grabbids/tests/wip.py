4ddcfa4e49aa92626a07d4a2aae4d8909d78142e
JB Poline <jbpoline@gmail.com>
2016-04-30 17:27:34 -0700

    wip

---
+++ grabbids/tests/wip.py
@@ -0,0 +1,24 @@
+import minify
+ls
+fn = './test_template.cjson'
+import numpy as np
+np.loadtxt(fn)
+with fh as open(fn):
+    print fh.read()
+fh = open(fn)
+fh.readlines()
+s = open(fn).readlines()
+minify.json_minify(s)
+s
+s[3]
+s.join()
+''.join(s)
+minify.json_minify(''.join(s))
+minify.json_minify(''.join(s), strip_space=False)
+fn
+import os.path as osp
+osp.basename(fn)
+osp.splitext(osp.basename(fn))
+osp.splitext(osp.basename(fn))[0]
+fnbase = osp.splitext(osp.basename(fn))
+fntuple = osp.splitext(osp.basename(fn))
