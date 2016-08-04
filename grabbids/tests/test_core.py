09c371b7c5fcd1c3be0db72bac9e6105a7a42506
tyarkoni <tyarkoni@gmail.com>
2016-08-04 10:33:50 -0700

    updated test

--- grabbids/tests/test_core.py
+++ grabbids/tests/test_core.py
@@ -8,5 +8,4 @@ def test_full():
     json_file = join(dirname(__file__), 'specs', 'test.json')
     data_dir = join(dirname(__file__), 'data', '7t_trt')
     struct = Structure(json_file, data_dir)
-    result = struct.get('subject', filter={'subject': '[1234]', 'run': '1'},
-                        flatten=True)
+    result = struct.get(filters={'subject': '[1234]', 'run': '1'})
