d42f185a7e0af719d7e291c760c384082282cd27
tyarkoni <tyarkoni@gmail.com>
2016-08-06 15:11:16 -0500

    adds File tests

--- grabbit/tests/test_core.py
+++ grabbit/tests/test_core.py
@@ -1,11 +1,41 @@
 import pytest
-from grabbit import Structure
-from os.path import join, dirname
+from grabbit import File, Entity, Layout
+import os
 from pprint import pprint
 
 
-def test_full():
-    json_file = join(dirname(__file__), 'specs', 'test.json')
-    data_dir = join(dirname(__file__), 'data', '7t_trt')
-    struct = Structure(json_file, data_dir)
-    result = struct.get(filters={'subject': '[1234]', 'run': '1'})
+@pytest.fixture
+def file():
+    path = os.path.join(os.path.dirname(__file__), 'data', '7t_trt', 'sub-03',
+                         'ses-2', 'func',
+                         'sub-03_ses-2_task-rest_acq-fullbrain_run-2_bold.nii.gz')
+    return(File(path))
+
+
+class TestFile:
+
+    def test_init(self):
+        path = os.path.join(os.path.dirname(__file__), 'data', '7t_trt',
+                            'sub-03', 'ses-2', 'func',
+                            'sub-03_ses-2_task-rest_acq-fullbrain_run-2_bold.nii.gz')
+        f = File(path)
+        assert os.path.exists(f.path)
+        assert f.filename is not None
+        assert os.path.isdir(f.dirname)
+        assert f.entities == {}
+
+    def test_matches(self, file):
+        assert file._matches()
+        assert file._matches(extensions='nii.gz')
+        assert not file._matches(extensions=['.txt', '.rtf'])
+        file.entities = {'task': 'rest', 'run': '2' }
+        assert file._matches(entities={'task': 'rest', 'run': 2})
+        assert not file._matches(entities={'task': 'rest', 'run': 4})
+
+    def test_named_tuple(self, file):
+        file.entities = {'attrA': 'apple', 'attrB': 'banana' }
+        tup = file.as_named_tuple()
+        assert(tup.filename == file.path)
+        assert isinstance(tup, tuple)
+        assert not hasattr(tup, 'task')
+        assert tup.attrA == 'apple'
