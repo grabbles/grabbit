dae4189181f0ff5517b8d2faf40909287eb99c67
Tal Yarkoni <tyarkoni@gmail.com>
2018-01-07 07:57:28 +0100

    add merge_layouts test

--- grabbit/tests/test_core.py
+++ grabbit/tests/test_core.py
@@ -1,9 +1,12 @@
 import pytest
-from grabbit import File, Entity, Layout
+from grabbit import File, Entity, Layout, merge_layouts
 import os
 import posixpath as psp
 import tempfile
 import json
+
+
+DIRNAME = os.path.dirname(__file__)
 
 
 @pytest.fixture
@@ -15,13 +18,13 @@ def file(tmpdir):
 
 
 @pytest.fixture(scope='module', params=['local', 'hdfs'])
-def layout(request):
+def bids_layout(request):
     if request.param == 'local':
-        root = os.path.join(os.path.dirname(__file__), 'data', '7t_trt')
+        root = os.path.join(DIRNAME, 'data', '7t_trt')
         # note about test.json:
         # in this test.json 'subject' regex was left to contain possible
         # leading 0; the other fields (run, session) has leading 0 stripped
-        config = os.path.join(os.path.dirname(__file__), 'specs', 'test.json')
+        config = os.path.join(DIRNAME, 'specs', 'test.json')
         return Layout(root, config, regex_search=True)
     else:
         hdfs = pytest.importorskip("hdfs")
@@ -31,11 +34,17 @@ def layout(request):
         config = psp.join('hdfs://localhost:9000{0}'.format(client.root), 'specs', 'test.json')
         return HDFSLayout(root, config, regex_search=True)
 
+@pytest.fixture(scope='module')
+def stamp_layout():
+    root = os.path.join(DIRNAME, 'data', 'valuable_stamps')
+    config = os.path.join(DIRNAME, 'specs', 'stamps.json')
+    return Layout(root, config)
+
 
 @pytest.fixture(scope='module')
 def layout_include(request):
-    root = os.path.join(os.path.dirname(__file__), 'data', '7t_trt')
-    config = os.path.join(os.path.dirname(__file__), 'specs', 'test_include.json')
+    root = os.path.join(DIRNAME, 'data', '7t_trt')
+    config = os.path.join(DIRNAME, 'specs', 'test_include.json')
     return Layout(root, config, regex_search=True)
 
 
@@ -107,25 +116,25 @@ class TestEntity:
 
 class TestLayout:
 
-    def test_init(self, layout):
-        if hasattr(layout, '_hdfs_client'):
-            assert layout._hdfs_client.list(layout.root)
+    def test_init(self, bids_layout):
+        if hasattr(bids_layout, '_hdfs_client'):
+            assert bids_layout._hdfs_client.list(bids_layout.root)
         else:
-            assert os.path.exists(layout.root)
-        assert isinstance(layout.files, dict)
-        assert isinstance(layout.entities, dict)
-        assert isinstance(layout.mandatory, set)
-        assert not layout.dynamic_getters
+            assert os.path.exists(bids_layout.root)
+        assert isinstance(bids_layout.files, dict)
+        assert isinstance(bids_layout.entities, dict)
+        assert isinstance(bids_layout.mandatory, set)
+        assert not bids_layout.dynamic_getters
 
-    def test_absolute_paths(self, layout):
-        result = layout.get(subject=1, run=1, session=1)
+    def test_absolute_paths(self, bids_layout):
+        result = bids_layout.get(subject=1, run=1, session=1)
         assert result  # that we got some entries
         assert all([os.path.isabs(f.filename) for f in result])
 
-        if not hasattr(layout, '_hdfs_client'):
-            root = os.path.join(os.path.dirname(__file__), 'data', '7t_trt')
+        if not hasattr(bids_layout, '_hdfs_client'):
+            root = os.path.join(DIRNAME, 'data', '7t_trt')
             root = os.path.relpath(root)
-            config = os.path.join(os.path.dirname(__file__), 'specs', 'test.json')
+            config = os.path.join(DIRNAME, 'specs', 'test.json')
 
             layout = Layout(root, config, absolute_paths=False)
 
@@ -140,8 +149,10 @@ class TestLayout:
 
         # Should always be absolute paths on HDFS
         else:
-            root = psp.join('hdfs://localhost:9000{0}'.format(layout._hdfs_client.root), 'data', '7t_trt')
-            config = psp.join('hdfs://localhost:9000{0}'.format(layout._hdfs_client.root), 'specs', 'test.json')
+            root = psp.join('hdfs://localhost:9000{0}'.format(
+                layout._hdfs_client.root), 'data', '7t_trt')
+            config = psp.join('hdfs://localhost:9000{0}'.format(
+                layout._hdfs_client.root), 'specs', 'test.json')
 
             layout = Layout(root, config, absolute_paths=False)
 
@@ -155,10 +166,12 @@ class TestLayout:
             assert all([os.path.isabs(f.filename) for f in result])
 
     @pytest.mark.parametrize('data_dir, config',
-                                [(os.path.join(os.path.dirname(__file__), 'data', '7t_trt'),
-                                 os.path.join(os.path.dirname(__file__), 'specs', 'test.json')),
-                                (psp.join('hdfs://localhost:9000/grabbit/test/', 'data', '7t_trt'),
-                                psp.join('hdfs://localhost:9000/grabbit/test/', 'specs', 'test.json'))])
+                             [(os.path.join(DIRNAME, 'data', '7t_trt'),
+                               os.path.join(DIRNAME, 'specs', 'test.json')),
+                              (psp.join('hdfs://localhost:9000/grabbit/test/',
+                               'data', '7t_trt'),
+                               psp.join('hdfs://localhost:9000/grabbit/test/',
+                               'specs', 'test.json'))])
     def test_dynamic_getters(self, data_dir, config):
 
         if ('hdfs' in data_dir or 'hdfs' in config):
@@ -168,72 +181,76 @@ class TestLayout:
         assert hasattr(layout, 'get_subjects')
         assert '01' in getattr(layout, 'get_subjects')()
 
-    def test_querying(self, layout):
+    def test_querying(self, bids_layout):
 
         # With regex_search = True (as set in Layout())
-        result = layout.get(subject=1, run=1, session=1, extensions='nii.gz')
+        result = bids_layout.get(subject=1, run=1, session=1,
+                                 extensions='nii.gz')
         assert len(result) == 8
-        result = layout.get(subject='01', run=1, session=1, type='phasediff',
-                            extensions='.json')
+        result = bids_layout.get(subject='01', run=1, session=1,
+                                 type='phasediff', extensions='.json')
         assert len(result) == 1
         assert 'phasediff.json' in result[0].filename
         assert hasattr(result[0], 'run')
         assert result[0].run == '1'
 
         # With exact matching...
-        result = layout.get(subject='1', run=1, session=1, extensions='nii.gz',
-                            regex_search=False)
+        result = bids_layout.get(subject='1', run=1, session=1,
+                                 extensions='nii.gz', regex_search=False)
         assert len(result) == 0
 
-        result = layout.get(target='subject', return_type='id')
+        result = bids_layout.get(target='subject', return_type='id')
         assert len(result) == 10
         assert '03' in result
-        result = layout.get(target='subject', return_type='dir')
+        result = bids_layout.get(target='subject', return_type='dir')
 
-        if hasattr(layout, '_hdfs_client'):
-            assert layout._hdfs_client.list(layout.root)
+        if hasattr(bids_layout, '_hdfs_client'):
+            assert bids_layout._hdfs_client.list(bids_layout.root)
         else:
             assert os.path.exists(result[0])
             assert os.path.isdir(result[0])
 
-        result = layout.get(target='subject', type='phasediff',
-                            return_type='file')
+        result = bids_layout.get(target='subject', type='phasediff',
+                                 return_type='file')
 
-        if hasattr(layout, '_hdfs_client'):
-            assert all([layout._hdfs_client.content(f) for f in result])
+        if hasattr(bids_layout, '_hdfs_client'):
+            assert all([bids_layout._hdfs_client.content(f) for f in result])
         else:
             assert all([os.path.exists(f) for f in result])
 
-    def test_natsort(self, layout):
-        result = layout.get(target='subject', return_type='id')
+    def test_natsort(self, bids_layout):
+        result = bids_layout.get(target='subject', return_type='id')
         assert result[:5] == list(map("%02d".__mod__, range(1, 6)))
 
-    def test_unique_and_count(self, layout):
-        result = layout.unique('subject')
+    def test_unique_and_count(self, bids_layout):
+        result = bids_layout.unique('subject')
         assert len(result) == 10
         assert '03' in result
-        assert layout.count('run') == 2
-        assert layout.count('run', files=True) > 2
+        assert bids_layout.count('run') == 2
+        assert bids_layout.count('run', files=True) > 2
 
-    def test_get_nearest(self, layout):
-        result = layout.get(subject='01', run=1, session=1, type='phasediff',
-                            extensions='.json', return_type='file')[0]
-        nearest = layout.get_nearest(result, type='sessions', extensions='tsv',
-                                     ignore_strict_entities=['type'])
+    def test_get_nearest(self, bids_layout):
+        result = bids_layout.get(
+            subject='01', run=1, session=1, type='phasediff',
+            extensions='.json', return_type='file')[0]
+        nearest = bids_layout.get_nearest(
+            result, type='sessions', extensions='tsv',
+            ignore_strict_entities=['type'])
         target = os.path.join('7t_trt', 'sub-01', 'sub-01_sessions.tsv')
         assert target in nearest
-        nearest = layout.get_nearest(result, extensions='tsv', all_=True,
-                                     ignore_strict_entities=['type'])
+        nearest = bids_layout.get_nearest(
+            result, extensions='tsv', all_=True,
+            ignore_strict_entities=['type'])
         assert len(nearest) == 3
-        nearest = layout.get_nearest(result, extensions='tsv', all_=True,
-                                     return_type='tuple',
-                                     ignore_strict_entities=['type'])
+        nearest = bids_layout.get_nearest(
+            result, extensions='tsv', all_=True, return_type='tuple',
+            ignore_strict_entities=['type'])
         assert len(nearest) == 3
         assert nearest[0].subject == '01'
 
-    def test_index_regex(self, layout, layout_include):
-        targ = os.path.join(layout.root, 'derivatives', 'excluded.json')
-        assert targ not in layout.files
+    def test_index_regex(self, bids_layout, layout_include):
+        targ = os.path.join(bids_layout.root, 'derivatives', 'excluded.json')
+        assert targ not in bids_layout.files
         targ = os.path.join(layout_include.root, 'models',
                             'excluded_model.json')
         assert targ not in layout_include.files
@@ -243,32 +260,32 @@ class TestLayout:
                                          'index': {'include': 'test',
                                                    'exclude': 'test'}})
 
-    def test_save_index(self, layout):
+    def test_save_index(self, bids_layout):
         tmp = tempfile.mkstemp(suffix='.json')[1]
-        layout.save_index(tmp)
+        bids_layout.save_index(tmp)
         assert os.path.exists(tmp)
         with open(tmp, 'r') as infile:
             index = json.load(infile)
-        assert len(index) == len(layout.files)
+        assert len(index) == len(bids_layout.files)
         # Check that entities for first 10 files match
         for i in range(10):
-            f = list(layout.files.values())[i]
+            f = list(bids_layout.files.values())[i]
             assert f.entities == index[f.path]
         os.unlink(tmp)
 
-    def test_load_index(self, layout):
-        f = os.path.join(os.path.dirname(__file__), 'misc', 'index.json')
-        layout.load_index(f)
-        assert layout.unique('subject') == ['01']
-        assert len(layout.files) == 24
+    def test_load_index(self, bids_layout):
+        f = os.path.join(DIRNAME, 'misc', 'index.json')
+        bids_layout.load_index(f)
+        assert bids_layout.unique('subject') == ['01']
+        assert len(bids_layout.files) == 24
 
         # Test with reindexing
-        f = os.path.join(os.path.dirname(__file__), 'misc', 'index.json')
-        layout.load_index(f, reindex=True)
-        assert layout.unique('subject') == ['01']
-        assert len(layout.files) == 24
+        f = os.path.join(DIRNAME, 'misc', 'index.json')
+        bids_layout.load_index(f, reindex=True)
+        assert bids_layout.unique('subject') == ['01']
+        assert len(bids_layout.files) == 24
 
-    def test_entity_mapper(self, layout):
+    def test_entity_mapper(self):
 
         class EntityMapper(object):
             def hash_file(self, file):
@@ -278,8 +295,8 @@ class TestLayout:
             def hash_file(self, file):
                 return str(hash(file.path)) + '.hsh'
 
-        root = os.path.join(os.path.dirname(__file__), 'data', '7t_trt')
-        config = os.path.join(os.path.dirname(__file__), 'specs',
+        root = os.path.join(DIRNAME, 'data', '7t_trt')
+        config = os.path.join(DIRNAME, 'specs',
                               'test_with_mapper.json')
 
         # Test with external mapper
@@ -299,6 +316,25 @@ class TestLayout:
         with pytest.raises(ValueError):
             layout = Layout(root, config, regex_search=True)
 
-    def test_merge_layouts(self, layout):
-        # TODO
-        pass
+    def test_clone(self, bids_layout):
+        lc = bids_layout.clone()
+        attrs = ['root', 'mandatory', 'dynamic_getters', 'regex_search',
+                 'filtering_regex', 'entity_mapper']
+        for a in attrs:
+            assert getattr(bids_layout, a) == getattr(lc, a)
+        assert bids_layout.files.keys() == lc.files.keys()
+        assert bids_layout.entities.keys() == lc.entities.keys()
+
+
+def test_merge_layouts(bids_layout, stamp_layout):
+    layout = merge_layouts([bids_layout, stamp_layout])
+    assert len(layout.files) == len(bids_layout.files) + \
+        len(stamp_layout.files)
+    assert 'country' in layout.entities
+    assert 'subject' in layout.entities
+
+    # Make sure first Layout was cloned and not passed by reference
+    patt = layout.entities['subject'].pattern
+    assert patt == bids_layout.entities['subject'].pattern
+    bids_layout.entities['subject'].pattern = "meh"
+    assert patt != "meh"
