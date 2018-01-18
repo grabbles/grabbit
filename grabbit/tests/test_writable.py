27dd19ad5ed32e8e7b48c8216c2a81416a66115d
Tal Yarkoni <tyarkoni@gmail.com>
2018-01-18 11:18:08 -0600

    reworked writable layouts

--- grabbit/tests/test_writable.py
+++ grabbit/tests/test_writable.py
@@ -1,5 +1,6 @@
 import pytest
-from grabbit import WritableFile, WritableLayout
+from grabbit import Layout, File
+from grabbit.extensions.writable import build_path
 import os
 import shutil
 from os.path import join, exists, islink, dirname
@@ -10,7 +11,7 @@ def writable_file(tmpdir):
     testfile = 'sub-03_ses-2_task-rest_acq-fullbrain_run-2_bold.nii.gz'
     fn = tmpdir.mkdir("tmp").join(testfile)
     fn.write('###')
-    return WritableFile(os.path.join(str(fn)))
+    return File(os.path.join(str(fn)))
 
 
 class TestWritableFile:
@@ -19,13 +20,11 @@ class TestWritableFile:
         writable_file.entities = {'task': 'rest', 'run': '2', 'subject': '3'}
 
         # Single simple pattern
-        with pytest.raises(ValueError):
-            writable_file.build_path()
+        with pytest.raises(TypeError):
+            build_path(writable_file.entities)
         pat = join(writable_file.dirname, '{task}/sub-{subject}/run-{run}.nii.gz')
         target = join(writable_file.dirname, 'rest/sub-3/run-2.nii.gz')
-        assert writable_file.build_path(pat) == target
-        writable_file.path_patterns = pat
-        assert writable_file.build_path() == target
+        assert build_path(writable_file.entities, pat) == target
 
         # Multiple simple patterns
         pats = ['{session}/{task}/r-{run}.nii.gz',
@@ -33,14 +32,14 @@ class TestWritableFile:
                 '{subject}/{task}.nii.gz']
         pats = [join(writable_file.dirname, p) for p in pats]
         target = join(writable_file.dirname, 't-rest/3-2.nii.gz')
-        assert writable_file.build_path(pats) == target
+        assert build_path(writable_file.entities, pats) == target
 
         # Pattern with optional entity
         pats = ['[{session}/]{task}/r-{run}.nii.gz',
                 't-{task}/{subject}-{run}.nii.gz']
         pats = [join(writable_file.dirname, p) for p in pats]
         target = join(writable_file.dirname, 'rest/r-2.nii.gz')
-        assert writable_file.build_path(pats) == target
+        assert build_path(writable_file.entities, pats) == target
 
     def test_build_file(self, writable_file, tmpdir, caplog):
         writable_file.entities = {'task': 'rest', 'run': '2', 'subject': '3'}
@@ -49,27 +48,27 @@ class TestWritableFile:
         new_dir = join(writable_file.dirname, 'rest')
         pat = join(writable_file.dirname, '{task}/sub-{subject}/run-{run}.nii.gz')
         target = join(writable_file.dirname, 'rest/sub-3/run-2.nii.gz')
-        writable_file.build_file(pat)
+        writable_file.copy(pat)
         assert exists(target)
 
         # Conflict handling
         with pytest.raises(ValueError):
-            writable_file.build_file(pat)
+            writable_file.copy(pat)
         with pytest.raises(ValueError):
-            writable_file.build_file(pat, conflicts='fail')
-        writable_file.build_file(pat, conflicts='skip')
+            writable_file.copy(pat, conflicts='fail')
+        writable_file.copy(pat, conflicts='skip')
         log_message = caplog.records[0].message
         assert log_message == 'A file at path {} already exists, ' \
                               'skipping writing file.'.format(target)
-        writable_file.build_file(pat, conflicts='append')
+        writable_file.copy(pat, conflicts='append')
         append_target = join(writable_file.dirname, 'rest/sub-3/run-2_1.nii.gz')
         assert exists(append_target)
-        writable_file.build_file(pat, conflicts='overwrite')
+        writable_file.copy(pat, conflicts='overwrite')
         assert exists(target)
         shutil.rmtree(new_dir)
 
         # Symbolic linking
-        writable_file.build_file(pat, symbolic_link=True)
+        writable_file.copy(pat, symbolic_link=True)
         assert islink(target)
         shutil.rmtree(new_dir)
 
@@ -77,12 +76,12 @@ class TestWritableFile:
         root = str(tmpdir.mkdir('tmp2'))
         pat = join(root, '{task}/sub-{subject}/run-{run}.nii.gz')
         target = join(root, 'rest/sub-3/run-2.nii.gz')
-        writable_file.build_file(pat, root=root)
+        writable_file.copy(pat, root=root)
         assert exists(target)
 
         # Copy into directory functionality
         pat = join(writable_file.dirname, '{task}/')
-        writable_file.build_file(pat)
+        writable_file.copy(pat)
         target = join(writable_file.dirname, 'rest', writable_file.filename)
         assert exists(target)
         shutil.rmtree(new_dir)
@@ -93,13 +92,13 @@ class TestWritableLayout:
     def test_write_files(self, tmpdir):
         data_dir = join(dirname(__file__), 'data', '7t_trt')
         config = join(dirname(__file__), 'specs', 'test.json')
-        layout = WritableLayout(data_dir, config=config)
+        layout = Layout(data_dir, config=config)
         pat = join(str(tmpdir), 'sub-{subject}'
                                 '/sess-{session}'
                                 '/r-{run}'
                                 '/type-{type}'
                                 '/task-{task}.nii.gz')
-        layout.write_files(path_patterns=pat)
+        layout.copy_files(path_patterns=pat)
         example_file = join(str(tmpdir), 'sub-02'
                                          '/sess-2'
                                          '/r-1'
@@ -111,7 +110,7 @@ class TestWritableLayout:
         contents = 'test'
         data_dir = join(dirname(__file__), 'data', '7t_trt')
         config = join(dirname(__file__), 'specs', 'test.json')
-        layout = WritableLayout(data_dir, config=config)
+        layout = Layout(data_dir, config=config)
         entities = {'subject': 'Bob', 'session': '01'}
         pat = join('sub-{subject}/sess-{session}/desc.txt')
         layout.write_contents_to_file(entities, path_patterns=pat,
@@ -128,7 +127,7 @@ class TestWritableLayout:
         contents = 'test'
         data_dir = join(dirname(__file__), 'data', '7t_trt')
         config = join(dirname(__file__), 'specs', 'test.json')
-        layout = WritableLayout(data_dir, config=[config, {
+        layout = Layout(data_dir, config=[config, {
             'default_path_patterns': ['sub-{subject}/ses-{session}/{subject}'
                                       '{session}{run}{type}{task}{acquisition}'
                                       '{bval}']
